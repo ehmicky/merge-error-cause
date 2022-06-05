3f3c533b6c3bf4f9bf5258d5ea7adcd79f68e76f
ehmicky <ehmicky@gmail.com>
2022-06-05 15:44:15 +0200

    Add more tests

--- test/main.js
+++ test/main.js
@@ -1,11 +1,15 @@
 import test from 'ava'
 import mergeErrorCause from 'merge-error-cause'
+import { each } from 'test-each'
 
 test('Error without cause is left as is', (t) => {
   const error = new Error('test')
   t.is(mergeErrorCause(error), error)
 })
 
-test('Error is normalized', (t) => {
-  t.true(mergeErrorCause() instanceof Error)
+// eslint-disable-next-line unicorn/no-null
+each([undefined, null, '', new Set([])], ({ title }, error) => {
+  test(`Error is normalized | ${title}`, (t) => {
+    t.true(mergeErrorCause(error) instanceof Error)
+  })
 })
