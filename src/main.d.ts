4d6ac25e8047b5c470a0f16d9a18d64ba95279aa
ehmicky <ehmicky@gmail.com>
2022-06-05 17:21:01 +0200

    Update TypeScript documentation

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,7 +1,42 @@
 /**
+ * This merges
+ * [`error.cause`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/cause)
+ * recursively with its parent `error`, including its
+ * [`message`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/message),
+ * [`stack`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/stack),
+ * [`name`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/name)
+ * and
+ * [`errors`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/AggregateError).
  *
  * @example
  * ```js
+ * const main = function (userId) {
+ *   try {
+ *     return createUser(userId)
+ *   } catch (error) {
+ *     throw mergeErrorCause(error)
+ *     // Printed as:
+ *     //   TypeError: Invalid user id: false
+ *     //   Could not create user.
+ *   }
+ * }
+ *
+ * const createUser = function (userId) {
+ *   try {
+ *     validateUserId(userId)
+ *     return sendDatabaseRequest('create', userId)
+ *   } catch (cause) {
+ *     throw new Error('Could not create user.', { cause })
+ *   }
+ * }
+ *
+ * const validateUserId = function (userId) {
+ *   if (typeof userId !== 'string') {
+ *     throw new TypeError(`Invalid user id: ${userId}.`)
+ *   }
+ * }
+ *
+ * main(false)
  * ```
  */
 export default function mergeErrorCause(error: any): Error
