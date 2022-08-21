e45b093107ba6cf85c690299f0ce84d8561456a4
ehmicky <ehmicky@gmail.com>
2022-08-21 15:41:03 +0200

    Start reworking stack logic

--- src/main.js
+++ src/main.js
@@ -4,14 +4,17 @@ import { mergeAggregateCauses, mergeAggregateErrors } from './aggregate.js'
 import { mergeMessage } from './message.js'
 import { copyProps } from './props.js'
 import { mergePrototype } from './prototype.js'
-import { getStackIndex, fixStack } from './stack.js'
+import { listStacks, getGenerated, fixStack } from './stack.js'
 
 // Merge `error.cause` recursively to a single error.
 // In Node <16.9.0 and in some browsers, `error.cause` requires a polyfill like
 // `error-cause-ponyfill`.
 export default function mergeErrorCause(error) {
-  const stackIndex = getStackIndex(error)
-  return mergeError(error, stackIndex)
+  const oldStacks = listStacks(error)
+  const errorA = normalizeException(error)
+  const newStacks = listStacks(errorA)
+  const generated = getGenerated(oldStacks, newStacks)
+  return mergeError(errorA, generated, 0)
 }
 
 // The recursion is applied pair by pair, as opposed to all errors at once.
@@ -19,29 +22,28 @@ export default function mergeErrorCause(error) {
 //    applied along the stack trace
 // It is applied in `error.cause`, but not in `error.errors` which have their
 // own stack.
-const mergeError = function (error, stackIndex) {
-  const parent = normalizeException(error)
+const mergeError = function (parent, generated, stackIndex) {
   mergeAggregateCauses(parent, mergeErrorCause)
-  return mergeCause(parent, stackIndex)
+  return mergeCause(parent, generated, stackIndex)
 }
 
 // `normalizeException()` is called again to ensure the new `name|message` is
 // reflected in `error.stack`.
-const mergeCause = function (parent, stackIndex) {
+const mergeCause = function (parent, generated, stackIndex) {
   if (parent.cause === undefined) {
     return parent
   }
 
-  const child = mergeError(parent.cause, stackIndex - 1)
+  const child = mergeError(parent.cause, generated, stackIndex + 1)
   // eslint-disable-next-line fp/no-delete, no-param-reassign
   delete parent.cause
-  mergeChild(parent, child, stackIndex)
+  mergeChild({ parent, child, generated, stackIndex })
   return normalizeException(parent)
 }
 
-const mergeChild = function (parent, child, stackIndex) {
+const mergeChild = function ({ parent, child, generated, stackIndex }) {
   mergeMessage(parent, child)
-  fixStack(parent, child, stackIndex)
+  fixStack({ parent, child, generated, stackIndex })
   mergeAggregateErrors(parent, child)
   copyProps(parent, child)
   mergePrototype(parent, child)
