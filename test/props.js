dba64686ad17988b8a0f8c8695e086753e39b49b
ehmicky <ehmicky@gmail.com>
2022-08-21 19:44:17 +0200

    Remove a test

--- test/props.js
+++ test/props.js
@@ -34,14 +34,3 @@ test('Symbol error properties are merged', (t) => {
   mergeErrorCause(error)
   t.true(error[symbol])
 })
-
-test('Inherited error properties are not merged', (t) => {
-  const TestError = class extends Error {
-    // eslint-disable-next-line class-methods-use-this
-    testFunc() {}
-  }
-  const error = new TypeError('test')
-  error.cause = new TestError('cause')
-  mergeErrorCause(error)
-  t.false('testFunc' in error)
-})
