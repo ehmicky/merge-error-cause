66ecde169f3a7ae7dc0ce2ffda200a97ca623114
ehmicky <ehmicky@gmail.com>
2022-08-28 15:18:24 +0200

    Split file

--- test/class/main.js
+++ test/class/main.js
@@ -24,63 +24,6 @@ test("Aggregate errors' classes are kept", (t) => {
   t.is(error.errors[0].name, 'RangeError')
 })
 
-test('Child class is used if parent is Error', (t) => {
-  const error = new Error('test')
-  error.cause = new RangeError('cause')
-  mergeErrorCause(error)
-  t.is(error.name, 'RangeError')
-})
-
-test('Child class is used if parent.wrap true', (t) => {
-  const error = new TypeError('test')
-  error.cause = new RangeError('cause')
-  error.wrap = true
-  mergeErrorCause(error)
-  t.is(error.name, 'RangeError')
-  t.false('wrap' in error)
-})
-
-test('Child class is not used if parent.wrap false', (t) => {
-  const error = new TypeError('test')
-  error.cause = new RangeError('cause')
-  error.wrap = false
-  mergeErrorCause(error)
-  t.is(error.name, 'TypeError')
-  t.false('wrap' in error)
-})
-
-test('Child class is not used if parent.wrap not boolean', (t) => {
-  const error = new TypeError('test')
-  error.cause = new RangeError('cause')
-  error.wrap = 'true'
-  mergeErrorCause(error)
-  t.is(error.name, 'TypeError')
-  t.is(error.wrap, 'true')
-})
-
-test('parent.wrap has priority over Error name', (t) => {
-  const error = new Error('test')
-  error.cause = new RangeError('cause')
-  error.wrap = false
-  mergeErrorCause(error)
-  t.is(error.name, 'Error')
-  t.false('wrap' in error)
-})
-
-// eslint-disable-next-line fp/no-class
-class WrapError extends Error {}
-// eslint-disable-next-line fp/no-mutating-assign
-Object.assign(WrapError.prototype, { name: 'WrapError', wrap: true })
-
-test('parent.wrap can be set on the error prototype', (t) => {
-  const error = new WrapError('test')
-  error.cause = new RangeError('cause')
-  mergeErrorCause(error)
-  t.is(error.name, 'RangeError')
-  t.false('wrap' in error)
-  t.true(WrapError.prototype.wrap)
-})
-
 // eslint-disable-next-line fp/no-class
 class TestError extends Error {
   constructor(...args) {
