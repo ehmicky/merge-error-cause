86245422407e7af6b9c784d191471aa8a60d8995
ehmicky <ehmicky@gmail.com>
2022-09-04 19:23:57 +0200

    Add more tests

--- test/main.js
+++ test/main.js
@@ -2,18 +2,6 @@ import test from 'ava'
 import mergeErrorCause from 'merge-error-cause'
 import { each } from 'test-each'
 
-test('Error without cause is left as is', (t) => {
-  const error = new TypeError('test')
-  mergeErrorCause(error)
-  t.false('cause' in error)
-})
-
-test('Argument is returned', (t) => {
-  const error = new TypeError('test')
-  error.cause = new TypeError('cause')
-  t.is(mergeErrorCause(error), error)
-})
-
 // eslint-disable-next-line unicorn/no-null
 each([undefined, null, '', new Set([])], ({ title }, error) => {
   test(`Error is normalized | ${title}`, (t) => {
@@ -21,45 +9,52 @@ each([undefined, null, '', new Set([])], ({ title }, error) => {
   })
 })
 
-test('Error cause is merged', (t) => {
-  const error = new TypeError('test')
-  error.cause = new TypeError('cause')
-  mergeErrorCause(error)
-  t.is(error.message, 'cause\ntest')
-  t.false('cause' in error)
-})
+each([Error, TypeError], ({ title }, ErrorClass) => {
+  test(`Error without cause is left as is | ${title}`, (t) => {
+    const error = new ErrorClass('test')
+    t.false('cause' in mergeErrorCause(error))
+  })
 
-test('Error cause is merged deeply', (t) => {
-  const error = new TypeError('test')
-  error.cause = new TypeError('cause')
-  error.cause.cause = new TypeError('innerCause')
-  mergeErrorCause(error)
-  t.is(error.message, 'innerCause\ncause\ntest')
-})
+  test(`Error cause is merged | ${title}`, (t) => {
+    const error = new ErrorClass('test')
+    error.cause = new TypeError('cause')
+    const returnError = mergeErrorCause(error)
+    t.is(returnError.message, 'cause\ntest')
+    t.false('cause' in returnError)
+  })
 
-test('Handle cause cycles', (t) => {
-  const error = new TypeError('test')
-  error.cause = error
-  mergeErrorCause(error)
-  t.false('cause' in error)
-  t.is(error.message, 'test')
-})
+  test(`Error cause is merged deeply | ${title}`, (t) => {
+    const error = new ErrorClass('test')
+    error.cause = new TypeError('cause')
+    error.cause.cause = new TypeError('innerCause')
+    t.is(mergeErrorCause(error).message, 'innerCause\ncause\ntest')
+  })
 
-test('Handle aggregate errors cycles', (t) => {
-  const error = new TypeError('test')
-  error.errors = [error]
-  mergeErrorCause(error)
-  t.is(error.errors.length, 0)
-  t.is(error.message, 'test')
-})
+  test(`Handle cause cycles | ${title}`, (t) => {
+    const error = new ErrorClass('test')
+    error.cause = error
+    const returnError = mergeErrorCause(error)
+    t.false('cause' in returnError)
+    t.is(returnError.message, 'test')
+  })
 
-test('Handle cause and aggregate errors cycles', (t) => {
-  const error = new TypeError('test')
-  error.cause = new TypeError('cause')
-  error.cause.errors = [new TypeError('innerCause'), error]
-  error.cause.errors[0].cause = error
-  mergeErrorCause(error)
-  t.is(error.message, 'cause\ntest')
-  t.is(error.errors.length, 1)
-  t.is(error.errors[0].message, 'innerCause')
+  test(`Handle aggregate errors cycles | ${title}`, (t) => {
+    const error = new ErrorClass('test')
+    error.errors = [error]
+    mergeErrorCause(error)
+    const { message, errors } = mergeErrorCause(error)
+    t.is(message, 'test')
+    t.is(errors.length, 0)
+  })
+
+  test(`Handle cause and aggregate errors cycles | ${title}`, (t) => {
+    const error = new ErrorClass('test')
+    error.cause = new TypeError('cause')
+    error.cause.errors = [new TypeError('innerCause'), error]
+    error.cause.errors[0].cause = error
+    const { message, errors } = mergeErrorCause(error)
+    t.is(message, 'cause\ntest')
+    t.is(errors.length, 1)
+    t.is(errors[0].message, 'innerCause')
+  })
 })
