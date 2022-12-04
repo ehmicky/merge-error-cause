ccc9668406afb7f27007c9bb93b3f1cdc10142c2
ehmicky <ehmicky@gmail.com>
2022-12-04 23:18:47 +0100

    Refactoring

--- src/main.test.js
+++ src/main.test.js
@@ -58,3 +58,84 @@ each([TypeError, Error], ({ title }, ErrorClass) => {
     t.is(errors[0].message, 'innerCause')
   })
 })
+
+each([true, false], ({ title }, wrap) => {
+  test(`Parent and child names are kept | ${title}`, (t) => {
+    const error = new TypeError('test')
+    const cause = new RangeError('cause')
+    error.cause = cause
+    error.wrap = wrap
+    mergeErrorCause(error)
+    t.is(error.name, 'TypeError')
+    t.is(cause.name, 'RangeError')
+  })
+
+  test(`Parent and child names are kept even when mismatched | ${title}`, (t) => {
+    const error = new TypeError('test')
+    error.name = 'RangeError'
+    const cause = new TypeError('cause')
+    error.cause = cause
+    error.cause.name = 'RangeError'
+    error.wrap = wrap
+    mergeErrorCause(error)
+    t.is(error.name, 'RangeError')
+    t.is(cause.name, 'RangeError')
+  })
+
+  test(`Aggregate errors' names are kept | ${title}`, (t) => {
+    const error = new TypeError('test')
+    error.cause = new RangeError('cause')
+    error.cause.errors = [new RangeError('innerError')]
+    error.wrap = wrap
+    t.is(mergeErrorCause(error).errors[0].name, 'RangeError')
+  })
+})
+
+each([true, false], [true, false], ({ title }, wrap, resetStack) => {
+  test(`Parent and child names are kept in stack | ${title}`, (t) => {
+    const error = new TypeError('test')
+    const cause = new RangeError('cause')
+    error.cause = cause
+
+    if (resetStack) {
+      error.cause.stack = ''
+    }
+
+    error.wrap = wrap
+    mergeErrorCause(error)
+    t.true(error.stack.includes('TypeError'))
+    t.true(cause.stack.includes('RangeError'))
+  })
+})
+
+each([TypeError, Error], ({ title }, ErrorClass) => {
+  test(`Child error properties are merged | ${title}`, (t) => {
+    const error = new ErrorClass('test')
+    error.cause = new TypeError('cause')
+    error.cause.prop = true
+    t.true(mergeErrorCause(error).prop)
+  })
+
+  test(`Parent error properties are merged | ${title}`, (t) => {
+    const error = new ErrorClass('test')
+    error.cause = new TypeError('cause')
+    error.prop = true
+    t.true(mergeErrorCause(error).prop)
+  })
+
+  test(`Parent error properties are merged with priority | ${title}`, (t) => {
+    const error = new ErrorClass('test')
+    error.cause = new TypeError('cause')
+    error.prop = true
+    error.cause.prop = false
+    t.true(mergeErrorCause(error).prop)
+  })
+
+  test(`Symbol error properties are merged | ${title}`, (t) => {
+    const error = new ErrorClass('test')
+    error.cause = new TypeError('cause')
+    const symbol = Symbol('prop')
+    error.cause[symbol] = true
+    t.true(mergeErrorCause(error)[symbol])
+  })
+})
