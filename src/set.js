f0fd17f1f2b35b33d68331ad8568b09cba032367
ehmicky <ehmicky@gmail.com>
2024-11-01 18:54:54 +0000

    Fix linting

--- src/set.js
+++ src/set.js
@@ -1,5 +1,6 @@
 // Error properties are non-enumerable
 export const setErrorProperty = (error, propName, value) => {
+  // eslint-disable-next-line fp/no-mutating-methods
   Object.defineProperty(error, propName, {
     value,
     writable: true,
